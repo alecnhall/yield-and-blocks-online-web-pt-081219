1a198ac95603cb185a1e474e745e72fa4b9099c4
Alec Hall <alecnhall@gmail.com>
2019-08-13 20:38:58 +0000

    Automatically backed up by Learn

--- practice.rb
+++ practice.rb
@@ -12,18 +12,20 @@ end
 
 # the yield keyword will allow you to call the method with a block of code and pass the torch, or yield, to that block.
 
-# def yielding
-#   puts "the program is executiong the code inside the method"
-#   yield
-#   puts "now we are back in the method"
-# end 
+def yielding
+  puts "the program is executiong the code inside the method"
+  yield
+  puts "now we are back in the method"
+end 
 
 yielding { puts "the method has yielded to the block!" }
 
 # Yielding with parameters 
 
-def yielding(num)
+def yielding_with_arguments(num)
   puts "the program is executiong the code inside the method"
   yield(num)
   puts "now we are back in the method"
 end 
+
+yielding_with_arguments(2) { |i| puts i * 2 }
