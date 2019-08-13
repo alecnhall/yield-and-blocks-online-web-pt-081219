5c88c92af480375bd49f8293d0e2a025e2146c3c
Alec Hall <alecnhall@gmail.com>
2019-08-13 20:32:17 +0000

    Automatically backed up by Learn

--- practice.rb
+++ practice.rb
@@ -1,9 +1,9 @@
 ["Tim", "Tom", "Jim"].each do |name|
-  puts "Hi #{name}"
+  puts "Hi, #{name}"
 end
 
 ["Tim", "Tom", "Jim"].each do |name|
   if name.start_with?("T")
-    puts "Hi #{name}"
+    puts "Hi, #{name}"
   end
 end
